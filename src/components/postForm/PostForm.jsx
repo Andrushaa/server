6c996be4e09ce94ccaa855009a6368c68f52b536
Andrusha <andrew.ivanow505@gmail.com>
2021-10-06 23:04:09 +0300

    up

--- src/components/postForm/PostForm.jsx
+++ src/components/postForm/PostForm.jsx
@@ -3,24 +3,32 @@ import MyInput from "../UI/input/MyInput";
 import MyButton from "../UI/button/myButton";
 
 const PostForm = ({create}) => {
-    const [post, setPost] = useState({title: '', body:''})
+    const [post, setPost] = useState({name: '', email:'', body:''})
 
     const addNewPost = (e) => {
         e.preventDefault()
         const newPost = {
             ...post, id: Date.now()
         }
-        create(newPost)
-        setPost({title: '', body:''})
+        if (newPost.name !== '' && newPost.email !== '' && newPost.body !== '') {
+            create(newPost)
+            setPost({name: '', email:'', body:''})
+        }
     }
 
     return (
         <form>
             <MyInput
-                value={post.title}
-                onChange={e => setPost({...post, title: e.target.value})}
+                value={post.name}
+                onChange={e => setPost({...post, name: e.target.value})}
                 type="text"
                 placeholder="Название поста"
+            />
+            <MyInput
+                value={post.email}
+                onChange={e => setPost({...post, email: e.target.value})}
+                type="email"
+                placeholder="Введите email"
             />
             <MyInput
                 value={post.body}
